4baae3b646b7f25995c193ddb1093de091ace3e6
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-12-10 22:34:20 -0800

    api data added and converted to charts, google maps added, overal app functionality complete

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,15 +1,22 @@
 
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines'; // package for charts lines
-
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'; // package for charts lines
+import _ from 'lodash';
 
 // since we are not going to be make use of any component state we will keep this as a functional component install of class
 export default (props) => {
+
+    function average(data) {
+        return _.round(_.sum(data)/data.length); 
+    }    
+
     return (
         <div>
             <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
                 <SparklinesLine color={props.color} />
-            </Sparklines>  
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>  
         </div>
     )
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     
     renderWeather(cityData) { // render a single row/city
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temps) // map over api data for each city and go inside and get list-main-temperature (see api data)
+        const temps = cityData.list.map(weather => weather.main.temp); // map over api data for each city and go inside and get list-main-temperature (see api data)
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord; // find the coord obj and grab the lon and lat properties and assign to new variables called lon and lat (destructuring)
 
         return ( // cityData will be our response from our api
             <tr key={name}>
-                <td>{name}</td>
-                <td>
-                    <Chart data={temps} color="orange" />      
-                </td> 
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         );
     }
@@ -26,9 +30,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody> 
