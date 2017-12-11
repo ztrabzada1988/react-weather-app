8b229420b2f622e9131626e95fef66220c7a69f9
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-12-10 23:03:39 -0800

    minor css and UI update

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -17,9 +17,9 @@ class WeatherList extends Component {
         return ( // cityData will be our response from our api
             <tr key={name}>
                 <td><GoogleMap lon={lon} lat={lat} /></td>
-                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={temps} color="red" units="K" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
-                <td><Chart data={humidities} color="black" units="%" /></td>
+                <td><Chart data={humidities} color="blue" units="%" /></td>
             </tr>
         );
     }
